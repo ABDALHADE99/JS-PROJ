8eb7a4d2161c54a381e80a2dc31aed77d9a148d3
ABDALHADE99 <abdoabdo100010@gmail.com>
2025-09-13 21:47:58 +0200

    this is vere fierst codde

---
+++ Roll The Dice/script.js
@@ -0,0 +1,25 @@
+let imges = ["dice-01.svg","dice-02.svg","dice-03.svg","dice-04.svg","dice-05.svg","dice-06.svg"];
+
+let dice =document.querySelectorAll("img");
+
+// console.log(dice);
+
+function roll() {
+    dice.forEach(
+        function(die){
+            die.classList.add("shake");
+            // dice.classList.add(imges);
+        //         setTimeout(function() {
+        // die.classList.remove("shake");
+        // }, 3000);
+        }
+    )
+    let one = Math.floor(Math.random()*6);
+    let tow = Math.floor(Math.random()*6);
+    console.log(one,tow);
+    let totale = document.querySelector("#total").innerHTML = "المجموع "+ (2+(one + tow));
+
+    document.querySelector("#die-1").setAttribute("src",imges[one]);
+    document.querySelector("#die-2").setAttribute("src",imges[tow]);
+}
+roll()
